bc3aab3d6d4d0ce3ea03b2f62c8a32f6c3fe624a
Miguel Angel <dammiguelangel@gmail.com>
2022-04-19 10:31:30 +0200

    09-Animated CSS

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-  declarations: [],
   exports: [
     MatButtonModule,
-    MatGridListModule
-    
+    MatGridListModule,
+    MatCardModule,
+    MatNativeDateModule,
   ]
 })
 export class MaterialModule { }
