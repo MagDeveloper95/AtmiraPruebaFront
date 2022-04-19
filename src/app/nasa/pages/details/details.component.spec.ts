7d972d52d6ffd67cdc633a5e765c7acd07c1752c
Miguel Angel <dammiguelangel@gmail.com>
2022-04-19 20:55:36 +0200

    11-Jasmine and Karma finished

--- src/app/nasa/pages/details/details.component.spec.ts
+++ src/app/nasa/pages/details/details.component.spec.ts
@@ -32,23 +32,6 @@ describe('DetailsComponent', () => {
   it('DetailsComponent should send data to app-nasa-information', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('app-nasa-information').dataset.photo).toBe(component.photo);
-  });
-  it('should  ', () => {
-    let photo: NasaResponse = {
-      date: '2022-02-02',
-      explanation: '',
-      hdurl: '',
-      media_type: '',
-      service_version: '',
-      title: '',
-      url: '',
-    };
-    component.photo = photo;
-    history.pushState(photo, '', '');
-    component.ngOnInit();
-    //if history.state is not null, then it means that the component is loaded from the history
-    expect(history.state).toBe(photo);
-    
-  });       
+  });     
 
 });
